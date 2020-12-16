0bb972c75b89d963fbf5d293fec0c7944e06eef2
Ilham Syahid S <ilhamsyahids@gmail.com>
2020-12-16 10:10:38 +0700

    add spinner loading

--- components/WrapperCoins.js
+++ components/WrapperCoins.js
@@ -1,22 +1,46 @@
 import React from 'react';
-import { ScrollView } from 'react-native'
+import { View, StyleSheet, ScrollView } from 'react-native'
+import Spinner from 'react-native-loading-spinner-overlay'
 
 import CoinCard from './CoinCard';
 
-const styles = {
+const styles = StyleSheet.create({
     contentContainer: {
         paddingBottom: 100,
         paddingTop: 55
     }
-}
+})
 
 class WrapperCoins extends React.Component {
+
+    state = {
+        loading: false
+    }
+
+    componentDidMount() {
+        this.setState({ loading: true })
+        setTimeout(() => this.setState({ loading: false }), 1000)
+    }
 
     renderCards() {
         return "1234567890".split('').map(card => <CoinCard key={card} />)
     }
 
     render() {
+        const { loading } = this.state
+        if (this.state.loading) {
+            return (
+                <View>
+                    <Spinner
+                        visible={loading}
+                        textContent="Loading..."
+                        textStyle={{ color: '#253145' }}
+                        animation="fade"
+                    />
+                </View>
+            )
+        }
+
         return (
             <ScrollView contentContainerStyle={styles.contentContainer}>
                 {this.renderCards()}
